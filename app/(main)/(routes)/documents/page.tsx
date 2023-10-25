ad5b5852843f7541b82a181c9a79d1c4e5917d10
Timo Heman <timoheman16@gmail.com>
2023-10-25 15:12:16 +0100

    added create and get documents api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,27 @@
 
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { useMutation } from "convex/react";
 import { useUser } from "@clerk/clerk-react";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 
 function DocumentPage() {
 	const { user } = useUser();
+	const create = useMutation(api.documents.create);
+
+	const onCreate = () => {
+		const promise = create({ title: "Untitled" });
+
+		toast.promise(promise, {
+			loading: "Creating a new note...",
+			success: "New note created!",
+			error: "Failed to create a new note.",
+		});
+	};
+
 	return (
 		<div className='h-full flex flex-col items-center justify-center space-y-4'>
 			<Image
@@ -25,7 +40,7 @@ function DocumentPage() {
 				className='hidden dark:block'
 			/>
 			<h2>Welcome to {user?.firstName}&apos;s Jotion.</h2>
-			<Button>
+			<Button onClick={onCreate}>
 				<PlusCircle className='h-4 w-4 mr-2' /> Create a note
 			</Button>
 		</div>
