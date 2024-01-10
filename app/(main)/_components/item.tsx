29e58e3a14057fe7476a8e1c7c2d562d7dd55dd5
Timo Heman <timoheman16@gmail.com>
2024-01-10 10:37:43 +0100

    added archive functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import {
+	ChevronDown,
+	ChevronRight,
+	LucideIcon,
+	MoreHorizontal,
+	Plus,
+	Trash,
+} from "lucide-react";
 import { useMutation } from "convex/react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
@@ -9,6 +16,14 @@ import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { useUser } from "@clerk/clerk-react";
 
 interface ItemProps {
 	id?: Id<"documents">;
@@ -36,8 +51,10 @@ function Item({
 	onExpand,
 }: ItemProps) {
 	const create = useMutation(api.documents.create);
+	const archive = useMutation(api.documents.archive);
 	const router = useRouter();
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+	const { user } = useUser();
 
 	const handleExpand = (
 		event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -47,7 +64,7 @@ function Item({
 	};
 
 	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		event.stopPropagation;
+		event.stopPropagation();
 		if (!id) return;
 
 		const promise = create({ title: "Untitled", parentDocument: id }).then(
@@ -63,6 +80,19 @@ function Item({
 			loading: "Creating a new note...",
 			success: "New note created",
 			error: "Failed to create new note",
+		});
+	};
+
+	const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation();
+		if (!id) return;
+
+		const promise = archive({ id });
+
+		toast.promise(promise, {
+			loading: "Moving to trash...",
+			success: "Note moved to trash",
+			error: "Failed to archive note.",
 		});
 	};
 
@@ -100,6 +130,31 @@ function Item({
 			)}
 			{!!id && (
 				<div className='ml-auto flex items-center gap-x-2'>
+					<DropdownMenu>
+						<DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+							<div
+								role='button'
+								className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-50-600'
+							>
+								<MoreHorizontal className='h-4 w-4 text-muted-foreground' />
+							</div>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							className='w-60'
+							align='start'
+							side='right'
+							forceMount
+						>
+							<DropdownMenuItem onClick={onArchive}>
+								<Trash className='h-4 w-4 mr-2' />
+								Delete
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<div className='text-xs text-muted-foreground p-2'>
+								Last edited by: {user?.fullName}
+							</div>
+						</DropdownMenuContent>
+					</DropdownMenu>
 					<div
 						role='button'
 						onClick={onCreate}
