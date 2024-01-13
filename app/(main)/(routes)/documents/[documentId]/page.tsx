505f060998aaa9dd9cd06041d7058f78c14fd4c7
Timo Heman <timoheman16@gmail.com>
2024-01-13 15:33:56 +0100

    added editor

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 import { Toolbar } from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps {
 	params: {
@@ -18,7 +20,29 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 		documentId: params.documentId,
 	});
 
-	if (document === undefined) return <div>Loading...</div>;
+	const update = useMutation(api.documents.update);
+
+	const onChange = (content: string) => {
+		update({
+			id: params.documentId,
+			content,
+		});
+	};
+
+	if (document === undefined)
+		return (
+			<div>
+				<Cover.Skeleton />
+				<div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
+					<div className='space-y-4 pl-8 pt-4'>
+						<Skeleton className='h-14 w-[50%]' />
+						<Skeleton className='h-4 w-[80%]' />
+						<Skeleton className='h-4 w-[40%]' />
+						<Skeleton className='h-4 w-[60%]' />
+					</div>
+				</div>
+			</div>
+		);
 
 	if (document === null) return <div>Not found</div>;
 	return (
@@ -26,6 +50,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 			<Cover url={document.coverImage} />
 			<div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
 				<Toolbar initialData={document} />
+				<Editor onChange={onChange} initialContent={document.content} />
 			</div>
 		</div>
 	);
